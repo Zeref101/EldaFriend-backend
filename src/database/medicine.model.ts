e80292eae15a91d76bd43bc7269788a37c3b8841
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-02 13:04:22 +0530

    v1 medicine model

---
+++ src/database/medicine.model.ts
@@ -0,0 +1,23 @@
+import { Schema, model, models, Document } from "mongoose";
+export interface IMedicine extends Document {
+  userId: Schema.Types.ObjectId;
+  name: string;
+  dosage: string;
+  duration: number;
+  startDate: Date;
+  isCompleted: [boolean];
+  scheduledTime: string;
+}
+
+const MedicineSchema = new Schema<IMedicine>({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true },
+  dosage: { type: String, required: true },
+  startDate: { type: Date, required: true, default: Date.now },
+  duration: { type: Number, required: true },
+  isCompleted: [Boolean],
+});
+
+const Medicine = models.Medicine || model("Medicine", MedicineSchema);
+
+export default Medicine;
