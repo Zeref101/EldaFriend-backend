61d832a537a79f445394a3d5067dd0900cfe179c
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-02 07:12:29 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+const corsOptions = {
+  origin: "",
+  methods: "GET, POST, PATCH, PUT, HEAD, DELETE",
+  credential: true,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello from Server");
+});
+
+app.listen(process.env.SERVER_PORT, () => console.log("Server is listening"));
