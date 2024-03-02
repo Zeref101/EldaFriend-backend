91494e8d3d394aab50c5ae442c02e26952a4f01b
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-02 13:04:11 +0530

    version 1 user model

---
+++ src/database/user.model.ts
@@ -0,0 +1,27 @@
+import { Schema, model, models, Document } from "mongoose";
+
+export interface IUser extends Document {
+  fullname: string;
+  phone: string;
+  email: string;
+  password: string;
+  picture: string;
+  groups: Schema.Types.ObjectId[];
+  medicines: Schema.Types.ObjectId[];
+  expenses: Schema.Types.ObjectId[];
+}
+
+const UserSchema = new Schema({
+  fullname: { type: String, reqired: true },
+  phone: { type: String, reqired: true, unique: true },
+  email: { type: String, reqired: true, unique: true },
+  password: { type: String, reqired: true },
+  picture: { type: String },
+  groups: [{ type: Schema.Types.ObjectId, ref: "Group" }],
+  medicines: [{ type: Schema.Types.ObjectId, ref: "Medicine" }],
+  expenses: [{ type: Schema.Types.ObjectId, ref: "Expense" }],
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
