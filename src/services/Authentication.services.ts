6f7842facd299e2a51c4493a8f33498b15e1076f
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-02 18:00:16 +0530

    nodemailer otp verification done

--- src/services/Authentication.services.ts
+++ src/services/Authentication.services.ts
@@ -2,6 +2,17 @@ import User from "../database/user.model";
 import { connectToDatabase } from "../lib/mongoose";
 import { hashPassword } from "../lib/util";
 import { CreateUserAltResponse, CreateUserParams } from "../types";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.SENDER_APP_PASSWORD,
+  },
+  port: 587,
+  host: "smtp.gmail.com",
+});
 
 export async function createUser({
   fullname,
@@ -31,11 +42,42 @@ export async function createUser({
 
     const hashedPassword = await hashPassword(password);
 
+    // ? GENERATING OTP
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    const mailOption = {
+      from: {
+        name: "Shreyas Mohanty",
+        address: "shreyasmohanty0228@gmail.com",
+      },
+      to: email,
+      subject: "Your OTP for signup",
+      text: "Your OTP is: " + otp,
+    };
+
     const user: any = await User.create({
       fullname: fullname,
       email: email,
       phone: phone,
       password: hashedPassword,
+      otp: otp,
+      verified: false,
+    });
+    transporter.sendMail(mailOption, (error, info) => {
+      if (error) {
+        console.log({
+          error: true,
+          status: 500,
+          message: "Error while sending the otp via email",
+        });
+        console.log(error);
+      } else {
+        console.log({
+          error: true,
+          status: 200,
+          message: "Please check your email for the otp",
+        });
+      }
     });
 
     if (!user) {
@@ -45,6 +87,45 @@ export async function createUser({
         message: `User not created due to an Internal server error`,
       };
     }
+
+    return user;
+  } catch (err) {
+    console.error(err);
+    return {
+      error: true,
+      status: 500,
+      message: "Internal server error",
+    };
+  }
+}
+
+export async function verifyOtp({
+  userId,
+  otp,
+}: {
+  userId: string;
+  otp: number;
+}): Promise<CreateUserParams | CreateUserAltResponse> {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return {
+        error: true,
+        status: 404,
+        message: "User not found",
+      };
+    }
+
+    if (user.otp !== otp) {
+      return {
+        error: true,
+        status: 400,
+        message: "Invalid OTP",
+      };
+    }
+
+    user.verified = true;
+    await user.save();
 
     return user;
   } catch (err) {
