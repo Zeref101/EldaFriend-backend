5253912732dab4277ee3723a6494ea003e53ccfb
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-02 16:04:54 +0530

    hashing funcition added

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,4 +1,11 @@
 import { z } from "zod";
+import bcrypt from "bcrypt";
+
+export async function hashPassword(password: string) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
 
 export const userSchema = z.object({
   fullname: z.string().min(1, { message: "Full name is required" }),
