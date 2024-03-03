f301b96f19b6cad3f720d484b3d7d17550ca073d
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-03 12:02:45 +0530

    model updated

--- src/database/medicine.model.ts
+++ src/database/medicine.model.ts
@@ -17,7 +17,8 @@ const MedicineSchema = new Schema<IMedicine>({
   dosageAmount: { type: Number, required: true },
   startDate: { type: Date, required: true, default: Date.now },
   duration: { type: Number, required: true },
-  isCompleted: [Boolean],
+  isCompleted: { type: [Boolean], required: true },
+  scheduledTime: { type: String, required: true },
 });
 
 const Medicine = models.Medicine || model("Medicine", MedicineSchema);
