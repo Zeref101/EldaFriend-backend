c11e8f73ae0275d264c13c5bf89673bdee0077bd
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-03 10:37:46 +0530

    model updated

--- src/database/medicine.model.ts
+++ src/database/medicine.model.ts
@@ -2,7 +2,8 @@ import { Schema, model, models, Document } from "mongoose";
 export interface IMedicine extends Document {
   userId: Schema.Types.ObjectId;
   name: string;
-  dosage: string;
+  dosageType: string;
+  dosageAmount: number;
   duration: number;
   startDate: Date;
   isCompleted: boolean[];
@@ -12,7 +13,8 @@ export interface IMedicine extends Document {
 const MedicineSchema = new Schema<IMedicine>({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
-  dosage: { type: String, required: true },
+  dosageType: { type: String, required: true },
+  dosageAmount: { type: Number, required: true },
   startDate: { type: Date, required: true, default: Date.now },
   duration: { type: Number, required: true },
   isCompleted: [Boolean],
