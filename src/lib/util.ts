de6253ce6a401ca4fce37961842058858a1014b9
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-03 12:05:32 +0530

    updated

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -6,11 +6,3 @@ export async function hashPassword(password: string) {
   const hashedPassword = await bcrypt.hash(password, saltRounds);
   return hashedPassword;
 }
-
-export const userSchema = z.object({
-  fullname: z.string().min(1, { message: "Full name is required" }),
-  email: z.string().email({ message: "Invalid email address" }),
-  password: z
-    .string()
-    .min(2, { message: "Password must be at least 2 character long" }),
-});
