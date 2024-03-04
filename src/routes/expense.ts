7277e058612a559ff71f854e38d42dd7a814dadf
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-04 21:09:27 +0530

    User expense field updated and tested

--- src/routes/expense.ts
+++ src/routes/expense.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response } from "express";
+import { Router, Request, Response } from "express";
 import { addExpenseSchema } from "../validations/expense.validation";
 import { addExpense } from "../services/expense.sevices";
+import User from "../database/user.model";
 
-const router = express.Router();
+const router = Router();
 
 router.post("/add-expense", async (req: Request, res: Response) => {
   const result = addExpenseSchema.safeParse(req.body);
@@ -16,19 +17,31 @@ router.post("/add-expense", async (req: Request, res: Response) => {
 
   try {
     if (!userId) {
-      res.status(404).json({
+      return res.status(404).json({
         message: "User ID not found",
       });
     }
     const newExpense = await addExpense(userId, itemName, itemPrice);
 
-    res.status(201).json({
-      message: "Expense added successfully",
-    });
+    if (newExpense.error) {
+      res.status(500).send(newExpense.message);
+    } else {
+      try {
+        await User.findByIdAndUpdate(userId, {
+          $push: { expenses: newExpense._id },
+        });
+        return res.status(201).send("Expense Updated successfully");
+      } catch (error) {
+        console.error(error);
+        return res
+          .status(500)
+          .json({ message: "Failed to update user with new expense" });
+      }
+    }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Failed to add expense" });
+    return res.status(500).json({ message: "Failed to add expense" });
   }
 });
 
-export default router;
+module.exports = router;
