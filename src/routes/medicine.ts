16fa63d2427ec638af98cc22667d8ebd2996e60d
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-04 21:09:49 +0530

    User medical field updated and tested

--- src/routes/medicine.ts
+++ src/routes/medicine.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import { createMedicine } from "../services/medicine.services";
 import { createMedSchema } from "../validations/medicine.validation";
+import User from "../database/user.model";
 
 const router = Router();
 
@@ -15,7 +16,7 @@ router.post("/create-med", async (req: Request, res: Response) => {
     }
 
     const { userId, dosageType, medAmount, medName, duration, scheduledTime } =
-      req.body;
+      result.data;
 
     const newMedicine = await createMedicine({
       userId,
@@ -29,11 +30,19 @@ router.post("/create-med", async (req: Request, res: Response) => {
     if (newMedicine.error) {
       return res.status(400).json({ message: newMedicine.error });
     }
-
-    return res.status(201).json({
-      message: "Medicine created successfully",
-      medicine: newMedicine,
-    });
+    try {
+      await User.findByIdAndUpdate(userId, {
+        $push: { medicines: newMedicine._id },
+      });
+      return res.status(201).json({
+        message: "Medicine created successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "Failed to create Medicine",
+      });
+    }
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
