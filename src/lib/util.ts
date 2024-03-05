b1b895b38f49af88877601dc873e013b87851f67
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-05 13:16:00 +0530

    updated

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,6 +1,4 @@
-import { z } from "zod";
 import bcrypt from "bcrypt";
-
 export async function hashPassword(password: string) {
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
