cb53ddedb9f3b95746ba39b7794bacaf56485922
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-05 13:15:51 +0530

    route for updating status for the meds created

--- src/routes/medicine.ts
+++ src/routes/medicine.ts
@@ -1,6 +1,12 @@
 import { Request, Response, Router } from "express";
-import { createMedicine } from "../services/medicine.services";
-import { createMedSchema } from "../validations/medicine.validation";
+import {
+  createMedicine,
+  updateIsCompleted,
+} from "../services/medicine.services";
+import {
+  createMedSchema,
+  updateIsCompletedSchema,
+} from "../validations/medicine.validation";
 import User from "../database/user.model";
 
 const router = Router();
@@ -46,6 +52,46 @@ router.post("/create-med", async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.patch("/med-taken", async (req: Request, res: Response) => {
+  try {
+    const result = updateIsCompletedSchema.safeParse(req.body);
+    if (!result.success) {
+      res.status(400).json({
+        message: result.error.issues.map((issue) => issue.message).join(","),
+      });
+    } else {
+      const { userId, medicineId, setTrueForDate, setTrue } = result.data;
+
+      const updateMedReponse = await updateIsCompleted({
+        userId,
+        medicineId,
+        setTrueForDate,
+        setTrue,
+      });
+
+      if (updateMedReponse?.error) {
+        return res
+          .status(updateMedReponse.status)
+          .send(updateMedReponse.message);
+      }
+      return res.sendStatus(204);
+    }
+  } catch (error) {
+    console.error("Error while updating medicine taken status", {
+      request: {
+        path: req.path,
+        method: req.method,
+        body: req.body,
+      },
+      error,
+    });
+
+    res.status(500).json({
+      message: "Internal Server error while updating the medicine taken status",
+    });
   }
 });
 
